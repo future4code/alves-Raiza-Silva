e769a41df4e94042b184e41f21908f56f9a313ec
Raiza gomes da silva <gomesraiza224@gmail.com>
2022-05-10 21:30:10 -0300

    exercicios aula de operadores

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,80 @@
+//exercicio 1
+
+// respostas: 
+
+/* let resultado = bool1 && bool2
+console.log("a.false", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b."true, resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c.false", resultado)
+
+console.log("d.undefined", typeof resultado) */
+
+//EXERCICIO 2: 
+
+/*let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) 
+
+ o erro esta imprimindo como uma strig  , teria que colocar como Number
+
+//EXERCICIO 3: 
+
+let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero =  Number(prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)*/
+
+//Exercícios de escrita de código
+
+// 1. 
+
+/* let suaIdade
+   let melhorAmigo
+
+  suaIdade = Number(prompt("Qual a sua idade"))
+  melhorAmigo = Number(prompt("Qual a idade do seu melhor amigo"))
+
+let resultado = true
+let resultado2= false
+
+console.log = Number(prompt("Sua idade é maior do que a do seu melhor amigo?" ))
+
+/* não entendi onde colocar true || false
+
+
+
+
+
+
+Exercícios 3 */
+
+/*let anoDeNascimento
+console.log = Number(prompt("qual seu ano de nascimento"))
+
+   a)let mesDeNascimento 
+console.log = Number(prompt("qual o mês que você nasceu")) */
+
+ // b)let diaDeNacimento
+ //console.log = Number(prompt("qual dia voce nasceu"))
+
+ // c)let horasQueNasceu
+ //console.log = Number(prompt("que horas voce nasceu"))
+
+
+//let digiteUmNumero 
+let digiteOutroNumero 
+
+
+//console.log= Number(prompt("digite 1 numero"))
+//console.log= Number(prompt("digite outro numero"))
+
+// SO CONSEGUIR FAZER ESSES :( 
