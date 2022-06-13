8e5625146476311c5e0d4b45dc4d6474052b0d2e
Raiza gomes da silva <gomesraiza224@gmail.com>
2022-06-12 22:07:35 -0300

    finalizado projeto whatslab

--- modulo2/projeto-whatslab/projeto-whatslab/src/components/Container.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/components/Container.js
@@ -6,33 +6,85 @@ export const ContainerStyled = styled.div `
  height: 75vh;
  border: 1px black solid;
  box-sizing: border-box;
+ background: Grey;
 
  
  `
-export const Quadro = styled.div `
+const Quadro = styled.div `
  border: 1px black solid;
  display: inline-block;
  
 `
-export const InputUsuario =styled.input `
+const InputUsuario =styled.input `
 width: 100px;
 `
-export const InputMsg = styled.input `
+const InputMsg = styled.input `
 width: 50vh;
 
 `
 
 export default class Conteiner extends React.Component{
-    render (){
+   state = {
+    mensagens: [
+        {
+            nome: "",
+            mensagem: ""
 
+        }
+    ],
+    valorName:"",
+    valorMensagem:""
+
+   }
+
+   enviar = ()=> {
+    const novaMsg = {
+        nome: this.state.valorName,
+        mensagem:this.state.valorMensagem
+    }
+    const novaMsgDois= [...this.state.mensagens, novaMsg]
+
+    this.setState({
+        mensagens: novaMsgDois,
+        valorName:"",
+        valorMensagem: ""
+
+
+    })
+   }
+
+   onChangeInputUsuario= (event)=>{
+    this.setState({valorName: event.target.value})
+   }
+  
+
+   onChangeInputMsg= (event)=>{
+    this.setState({valorMensagem: event.target.value})
+   }
+  
+    render (){
+            const listDeMsg = this.state.mensagens.map((chat, indice)=>{
+                return <div key={indice}>
+                    <p>{chat.nome}</p>
+                    <p>{chat.mensagem}</p>
+
+                </div>
+            }
+            )
         return(
+           
             <   Quadro>
+              
                <div>
-                <ContainerStyled ></ContainerStyled>
+                <ContainerStyled >
+                    {listDeMsg}
+                </ContainerStyled>
                 <div>
-                <InputUsuario type="text" placeholder='Usuario' value=""/>
-                <InputMsg type="text" placeholder='mensagem' value=""/>
-                <button>Enviar</button>
+                <InputUsuario type="text" placeholder='Usuario' value={this.state.valorName} onChange={this.onChangeInputUsuario}/>
+                <InputMsg type="text" placeholder='mensagem' value={this.state.valorMensagem} onChange={this.onChangeInputMsg}/>
+                <button onClick={this.enviar}> Enviar 
+                    <fiSend size={20} />
+                </button>
                 </div>
                </div>
             </  Quadro>
