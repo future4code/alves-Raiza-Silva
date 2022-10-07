f127577450cf870fd20be88933f2845baf4f3113
Raiza gomes da silva <gomesraiza224@gmail.com>
2022-10-07 20:00:05 -0300

    inicio

---
+++ Rodada-de-cases/front-end-challenge/frontend-personare/src/App.js
@@ -0,0 +1,32 @@
+//import axios from "axios";
+//import { useEffect, useState } from 'react';
+import Header from "./compoents/Header"
+
+
+
+export default function App() {
+
+
+  
+  return (
+    <div>
+      <Header />
+      <h1>TAROT DO DIA</h1>
+
+      <div>
+        <h3>CONCENTRE-SE PROFUNDAMENTE NO SEU DIA</h3>
+          <p>Escolha um momento tranquilo para o jogo e feche os olhos por alguns instantes.<strong>Concentre-se e peça mentalmente uma orientação para o seu dia.</strong>
+            Você só pode tirar uma carta, por isso, quando se sentir preparado embaralhe as cartas.</p>
+        <button>Embaralhar cartas</button>
+      </div>
+
+
+      <section>
+        <h3>SELECIONE UMA CARTA DO BARALHO ABAIXO</h3>
+        
+      </section>
+
+
+    </div>
+  );
+}
---
+++ Rodada-de-cases/front-end-challenge/frontend-personare/src/compoents/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import img from "../img/logo.png"
+
+export default function Header() {
+ 
+
+  return (
+    <div>
+   <img src={img}/>
+    </div>
+  );
+}
