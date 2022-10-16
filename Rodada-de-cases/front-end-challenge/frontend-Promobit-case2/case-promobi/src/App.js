e8129e6581c14d2387596b93c4f2032a3c926147
Raiza gomes da silva <gomesraiza224@gmail.com>
2022-10-16 18:44:18 -0300

    add urls e pastas de organização

--- Rodada-de-cases/front-end-challenge/frontend-Promobit-case2/case-promobi/src/App.js
+++ Rodada-de-cases/front-end-challenge/frontend-Promobit-case2/case-promobi/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header';
 import PageFilter from './pages/PageFilter/PageFilter';
+import PageHome from './pages/PageHome/PageHome';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div> 
       <Header/>
       <PageFilter/>
+      <PageHome/>
     </div> 
   );
 }
--- Rodada-de-cases/front-end-challenge/frontend-Promobit-case2/case-promobi/src/pages/PageHome/PageHome.js
+++ Rodada-de-cases/front-end-challenge/frontend-Promobit-case2/case-promobi/src/pages/PageHome/PageHome.js
@@ -1,8 +1,35 @@
 import React from "react"
+import axios from "axios"
+import {BASE_URL} from "../../constants/BASE_URL"
+import {API_KEY} from "../../constants/API_KEY"
+import {BASE_IMG} from "../../constants/BASE_IMG"
+
 
 export default class PageHome extends React.Component {
 
+    state = {
+        listFilmes : []
+    }
+
+    componentDidMount(){
+        this.getListFilme()
+   }
+
+    getListFilme = () => {
+        axios.get(`${BASE_URL}3/movie/popular?api_key=${API_KEY}`)
+        .then((res) => this.setState({listFilmes: res.data.results}))
+            
+        .catch((error) => {
+            console.log({ error })
+        })
+}
+
+    
+
     render() {
+        const renderList = this.state.listFilmes.map((filmes)=>{
+            return <div></div>
+        })
 
         return <div>
             <h1>Pagina home  </h1>
